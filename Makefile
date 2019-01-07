d9686b003cd57213b8c137fea679f59f4c6c9962
tong <tong@disktree.net>
2019-01-07 11:35:55 +0100

    ...

---
+++ Makefile
@@ -0,0 +1,35 @@
+PREFIX ?= /usr
+INSTALL_DIR ?= $(PREFIX)
+SRC = $(wildcard src/**/*.hx)
+
+all: build
+
+app/app.js: $(SRC) app.hxml
+	haxe app.hxml
+
+bin/letterserver.js: $(SRC) server.hxml
+	haxe server.hxml
+
+build: app/app.js bin/letterserver.js
+
+install:
+	mkdir -p $(INSTALL_DIR)/lib/letterserver
+	cp bin/package.json $(INSTALL_DIR)/lib/letterserver
+	cd $(INSTALL_DIR)/lib/letterserver && npm install
+	cp bin/letterserver.js* $(INSTALL_DIR)/lib/letterserver
+	chmod +x $(INSTALL_DIR)/lib/letterserver/letterserver.js
+	cp bin/letterserver.service /etc/systemd/system/
+	systemctl daemon-reload
+
+uninstall:
+	rm -rf $(INSTALL_DIR)/lib/letterserver
+	rm -f /etc/systemd/system/letterserver.service
+
+clean:
+	rm -f app/app.js
+	rm -f app/app.js.map
+	rm -f app/app.css
+	rm -f bin/letterserver.js
+	rm -f bin/letterserver.js.map
+
+.PHONY: all build install uninstall clean
--- src/letterspace/Server.hx
+++ src/letterspace/Server.hx
@@ -1,8 +1,8 @@
 package letterspace;
 
+/*
 #if owl_client
 
-/*
 class Mesh extends owl.Mesh {
 	override function createNode( id : String, ?configuration : js.html.rtc.Configuration, ?info : Dynamic ) : Node {
 		trace("createNode "+info);
@@ -21,45 +21,82 @@ class Node extends owl.Node {
 }
 */
 
+#if owl_server
+
+import om.System;
+import Sys.print;
+import Sys.println;
+
+class Mesh extends owl.Mesh {}
+class Node extends owl.Node {}
+
+#end
+
 class Server extends owl.Server {
 
-	/*TODO load json
+	#if owl_client
+	/*
+	//TODO load json
 	public static var HOSTS(default,null) = [
 		{ host:'' }
 	];
-	*/
 
-	/*
 	override function createMesh( id : String ) : Mesh {
 		trace("createMesh");
 		return new letterspace.Mesh( this, id );
 	}
 	*/
-}
+	#end
 
-#elseif owl_server
+	#if owl_server
 
-class Mesh extends owl.Mesh {}
-class Node extends owl.Node {}
-
-class Server extends owl.Server {
+	public static var isSystemService(default,null) = false;
+	public static var server(default,null) : Server;
 
 	override function createMesh( id : String ) : Mesh {
 		return new letterspace.Mesh( id );
 	}
 
+	static function exit( ?msg : Dynamic, code = 0 ) {
+		if( msg != null ) println( msg );
+		Sys.exit( code );
+	}
+
 	static function main() {
 
-		//'195.201.41.121';
-		var host = '192.168.0.10';
+		if( !System.is( 'linux' ) ) exit( 'linux only', 1 );
+
+		var host : String = null;
 		var port = 1377;
 
-		var server = new letterspace.Server( host, port );
-		server.start().then( function(_) {
-			trace('SERVER READY');
-			server.addMesh( new owl.Mesh('letterspace') );
-        });
-	}
-}
+		var argsHandler : {getDoc:Void->String,parse:Array<Dynamic>->Void};
+		argsHandler = hxargs.Args.generate([
+			["--service"] => function() isSystemService = true,
+			@doc("Host name")["-host"] => function(name:String) host = name,
+			@doc("Port number")["-port"] => function(number:Int) port = number,
+			@doc("Print usage")["--help"] => function() {
+				exit( 'Usage : letterserver <cmd> [params]\n'+argsHandler.getDoc(), 1 );
+			},
+			_ => function(arg) exit( 'Unknown parameter: $arg', 1 )
+		]);
+		argsHandler.parse( Sys.args() );
 
-#end
+		switch host {
+		case null,'auto':
+			host = om.Network.getLocalIP()[0];
+			if( host == null ) exit( 'failed to resolve host name', 1 );
+		default:
+		}
+
+		println( 'Starting server $host:$port' );
+
+		server = new letterspace.Server( host, port );
+		server.start().then( function(_) {
+			server.addMesh( new owl.Mesh('letterspace') );
+        }).catchError( function(e){
+			exit(e);
+		});
+	}
+
+	#end
+}
